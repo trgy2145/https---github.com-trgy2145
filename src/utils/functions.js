1e7a4aa04c552ab1ff289a9f9fcd347e971b6d3d
trgy2145 <trgy2145@gmail.com>
2022-03-24 14:14:37 +0100

    first commit

---
+++ src/utils/functions.js
@@ -0,0 +1,62 @@
+import firebase from "./firebase";
+import { useState, useEffect } from "react";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  onValue,
+  query,
+  remove,
+  update,
+} from "firebase/database";
+import { successNote } from "./customTostify";
+
+export const addInfo = (info) => {
+  const db = getDatabase();
+  const userRef = ref(db, "trgy");
+  const newUserRef = push(userRef);
+  set(newUserRef, {
+    username: info.username,
+    phoneNumber: info.phoneNumber,
+    gender: info.gender,
+  });
+  successNote("Added successfully");
+};
+
+export const useFetch = () => {
+  const [contactList, setContactList] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+
+    const db = getDatabase();
+    const userRef = ref(db, "trgy");
+
+    onValue(query(userRef), (snapshot) => {
+      const contacts = snapshot.val();
+      // send an array of the values in database
+      const contactArray = [];
+      for (let id in contacts) {
+        contactArray.push({ id, ...contacts[id] });
+      }
+      setContactList(contactArray);
+      setIsLoading(false);
+    });
+  }, []);
+  return { isLoading, contactList };
+};
+
+export const deleteInfo = (id) => {
+  const db = getDatabase();
+  // const userRef = ref(db, 'contact');
+  remove(ref(db, "trgy/" + id));
+  successNote("Deleted");
+};
+
+export const updateInfo = (info) => {
+  const db = getDatabase();
+  const updates = {};
+  updates["trgy/" + info.id] = info;
+  return update(ref(db), updates);
+};
